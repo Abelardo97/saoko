a66e6b2b8aad646ac6ade98848a0879120eccf8b
Abelardo Gutierrez <gabelardo04@gmail.com>
2022-09-26 10:53:26 -0500

    feat: uso de script externo

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-      console.log("Saoko papi Saoko");
-      document.getElementById("generation").style.color = "grey";
-      function colorTitulo() {
-        console.log("Pulso el boton de Generation");
-        document.getElementById("generation").style.color = "blue";
+    console.log("Saoko papi Saoko");
+    document.getElementById("generation").style.color = "grey";
+    function colorTitulo() {
+    console.log("Pulso el boton de Generation");
+    document.getElementById("generation").style.color = "blue";
     
       }
